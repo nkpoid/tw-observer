bc4887aa1f20471b4907fac6554e279c44473fc1
nkpoid <nkpoidjp@gmail.com>
2022-04-02 12:58:06 +0900

    update notification text

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ import json
 import logging
 import os
 import re
+from collections import OrderedDict
 from datetime import datetime
 from logging import getLogger
+from typing import List
 
 import requests
 import tweepy
 from apscheduler.schedulers.blocking import BlockingScheduler
 from pymongo import MongoClient
 
-scheduler = BlockingScheduler()
+scheduler = BlockingScheduler(timezone="Asia/Tokyo")
 logging.basicConfig(level=logging.INFO)
 
 
@@ -21,13 +23,14 @@ def load_jsonc(filepath: str):
     return json.loads(text_without_comment)
 
 
-@scheduler.scheduled_job("interval", minutes=3, next_run_time=datetime.now())  # type: ignore
+@scheduler.scheduled_job("interval", minutes=15, next_run_time=datetime.now())  # type: ignore
 def observe_task():
     logger = getLogger("tw-observer")
-    logger.info("Start observe...")
+    logger.info("Start observe task...")
 
     mc = MongoClient(os.environ["MONGODB_URI"])
     db = mc.tw_observer
+    notifications: List[str] = []
 
     tokens = load_jsonc("./tokens.jsonc")
     for token in tokens:
@@ -48,24 +51,32 @@ def observe_task():
             tweepy.Paginator(tc.get_users_followers, id=me.id, user_auth=me.protected, max_results=1000).flatten()
         )
         current_followers = [{"id": u.id, "username": u.username} for u in _current_followers]
-        diff = set([o["id"] for o in prev_followers]) - set([o["id"] for o in current_followers])
 
-        for user_id in diff:
-            res = tc.get_user(id=user_id)
-            if errors := res.errors:  # type:ignore
-                text = f'[**@{me.username}**] {errors[0]["detail"]}'
+        for pf in prev_followers:
+            userid = pf["id"]
+            cf = next(filter(lambda _cf: _cf["id"] == userid, current_followers), None)
+            if cf:
+                if pf["username"] != cf["username"]:
+                    notifications.append(f"@{pf['username']} has renamed to @{cf['username']}")
             else:
-                user: tweepy.User = res.data  # type:ignore
-                text = f"[@{user.username}](https://twitter.com/{user.username}) has been removed **@{me.username}**"
-
-            requests.post(
-                os.environ["WEBHOOK_URL"],
-                json.dumps({"embeds": [{"description": text}]}),
-                headers={"Content-Type": "application/json"},
-            )
+                _res = tc.get_user(id=userid)
+                if errors := _res.errors:  # type:ignore
+                    notifications.append(errors[0]["detail"])
+                else:
+                    user: tweepy.User = _res.data  # type:ignore
+                    notifications.append(
+                        f"[@{user.username}](https://twitter.com/{user.username}) has been removed **{me.username}**"
+                    )
 
         collection.delete_many({})
         collection.insert_many(current_followers)
+
+    if notifications:
+        requests.post(
+            os.environ["WEBHOOK_URL"],
+            json.dumps({"embeds": [{"description": "\n".join(list(OrderedDict.fromkeys(notifications)))}]}),
+            headers={"Content-Type": "application/json"},
+        )
 
     logger.info("Done.")
 
